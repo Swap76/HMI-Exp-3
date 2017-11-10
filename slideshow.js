b480b24ce5121c205be883621005750f310730b5
Nischal Shrestha <nischalsht7442@gmail.com>
2017-11-09 21:29:55 -0600

    animation on slides

--- slideshow.js
+++ slideshow.js
@@ -1,22 +1,41 @@
-	var slideIndex = 0;
-		showSlides();
+/* 
 
-		function showSlides() 
-		{
-    		var i;
-    		var slides = document.getElementsByClassName("mySlides");
-    		var dots = document.getElementsByClassName("dot");
-    		for (i = 0; i < slides.length; i++) 
-    		{
-       			slides[i].style.display = "none";  
-    		}
-    		slideIndex++;
-    		if (slideIndex > slides.length) {slideIndex = 1}    
-    		for (i = 0; i < dots.length; i++) 
-    		{
-        		dots[i].className = dots[i].className.replace(" active", "");
-    		}
-    		slides[slideIndex-1].style.display = "block";  
-    		dots[slideIndex-1].className += " active";
-    		setTimeout(showSlides, 3000); // Change image every 2 seconds
-		}
+Creating a Automatic Slideshow using Java Script.
+
+*/
+
+var slideIndex = 0;
+showSlides();
+
+function showSlides() 
+{
+	var i;
+
+    //Import the slides from HTML
+	var slides = document.getElementsByClassName("mySlides");
+
+    //Import the dots for the slideshow from HTML
+	var dots = document.getElementsByClassName("dot");
+
+	for (i = 0; i < slides.length; i++) 
+	{
+			slides[i].style.display = "none";  
+	}
+
+	slideIndex++;
+	if (slideIndex > slides.length) 
+    {
+        slideIndex = 1;
+    }
+
+	for (i = 0; i < dots.length; i++) 
+	{
+		dots[i].className = dots[i].className.replace("active", "");
+	}
+
+	slides[slideIndex-1].style.display = "block";  
+	dots[slideIndex-1].className += " active";
+
+    //Automatically changes slide every 3 seconds
+	setTimeout(showSlides, 3000);
+}
